b0e3acc1529991e26744cc2c766898934dda9602
Stormi <stormitalent@gmail.com>
2024-07-05 07:58:44 -0400

    change limit file size

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.use(json({limit:'50mb'}));
+  app.use(urlencorded({extended: true, limit: '50mb'}));
   await app.listen(3001);
 }
 bootstrap();
